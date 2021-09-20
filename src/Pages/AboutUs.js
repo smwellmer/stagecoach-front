fe963584c349a0d5a408c18ad99d6f3558dc93cc
Samantha Wellmer <sam@Samanthas-MacBook-Air.local>
2021-09-20 15:42:50 -0400

    about us

--- src/Pages/AboutUs.js
+++ src/Pages/AboutUs.js
@@ -3,16 +3,32 @@ import React from 'react'
 const AboutUs = (props) => {
     return (
         <div className='aboutUsContainer'>
-            <h1>About StageCoach Café</h1>
-            {/* <img className='img2' src="Photos/large_building.jpeg" alt="Us2"/>  */}
-            <img className='img3' src="Photos/cafe2.jpg" alt="Us3"/> 
-            <img className='img4' src="Photos/cafe1.jpg" alt="Us4"/> 
+            {/* <h1>About StageCoach Café</h1> */}
+            <h2>The Inspiration</h2>
+            <img className='img1' src="Photos/grandmafreda.jpeg" alt="freda"/> 
+            <img className='img2' src="Photos/piemamaw.jpeg" alt="vicki"/> 
+            
             <span>
-                The StageCoach Café was founded in 2016 by Andrea Barr. 
+               The two ladies pictured above greatly influenced what has come to be the StageCoach Café. 
+               Many recipes and even more inspiration come from these women and their kitchens.               
 
-                Scratch made comfort food at its best!Serving made to order breakfast and lunch. Daily specials, soups, sandwiches and homemade desserts.
-                We accept all major credit cards and cash.
+            </span>
 
+            <h2>The Founder & Chef</h2>
+            <img className='img5' src="Photos/mom.jpeg" alt="andrea"/> 
+
+            <span>
+              Andrea Wallingford Barr founded the StageCoach Café in 2016. 
+              She always had dreams of being able to share her love of great food and family recipes with others.  
+              In 5 short years, she has been able to build her business and create a wonderful community in New Carlisle.
+
+            </span>
+            <img className='img3' src="Photos/cafe2.jpg" alt="cafe1"/> 
+           <img className='img4' src="Photos/cafe1.jpg" alt="cafe2"/> 
+            <span>
+            The StageCoach Café is located just outside the center of New Carlisle. 
+            The café is rightly named, as the building used to be a stagecoach stop.
+            Now updated, with a cute "farmhouse" feel, the food and decor are both equally inviting!
             </span>
 
         </div> 
